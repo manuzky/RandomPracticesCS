be9627d433ecf9656c50c5c612f476ea165d8614
manuzky <manuelc.dev@gmail.com>
2025-10-01 01:51:21 -0400

    Upd 1: Convertor ºC <-> ºF

---
+++ RandomPracticesCS/ConvertCtoF.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomPracticesCS
+{
+    internal class ConvertCtoF
+    {
+        public void Convertor()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("-----------------------------------------------------------------------------");
+            Console.ResetColor();
+
+            string final;
+            do
+            {
+                Console.WriteLine("\nWhat do you want to convert?\n1. ºC to ºF   -   2. ºF to ºC");
+                int choice;
+                double fahrenheit, celsius;
+                while (!int.TryParse(Console.ReadLine(), out choice) || choice <= 0 || choice >= 3)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("INVALID ERROR: You have to choose a number:\n1. ºC to ºF   -   2. ºF to ºC");
+                    Console.ResetColor();
+                }
+
+                if (choice == 1)
+                {
+                    Console.WriteLine("\nYOU SELECTED ºC to ºF.");
+                    Console.WriteLine("Enter the ºC");
+                    while (!double.TryParse(Console.ReadLine(), out celsius))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("ERROR: You have to enter a number.");
+                        Console.ResetColor();
+                    }
+                    fahrenheit = (celsius * 1.8) + 32;
+                    Console.WriteLine($"{celsius}ºC are {Math.Round(fahrenheit, 2)}ºF");
+                }
+                else if (choice == 2)
+                {
+                    Console.WriteLine("\nYOU SELECTED ºF to ºC.");
+                    Console.WriteLine("Enter the ºF");
+                    while (!double.TryParse(Console.ReadLine(), out fahrenheit))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("ERROR: You have to enter a number.");
+                        Console.ResetColor();
+                    }
+                    celsius = (fahrenheit - 32) / 1.8;
+                    Console.WriteLine($"{fahrenheit}ºF are {Math.Round(celsius, 2)}ºC");
+                }
+
+                Console.WriteLine("\nDo you wanna try again the convertor?\nYes (Y) - No (N)");
+                final = Console.ReadLine().ToLower();
+                while (final != "yes" && final != "y" && final != "no" && final != "n")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"\nERROR: you must say Yes (Y) or No (N)");
+                    Console.ResetColor();
+                    final = Console.ReadLine().ToLower();
+                }
+            } while (final == "yes" || final == "y");
+
+
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("-----------------------------------------------------------------------------");
+            Console.ResetColor();
+        }
+    }
+}
--- RandomPracticesCS/Program.cs
+++ RandomPracticesCS/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Emit;
 
 namespace RandomPracticesCS
 {
@@ -6,7 +7,52 @@ namespace RandomPracticesCS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // WELCOME
+            Console.WriteLine("Hi! This is an example of different practices I'll be doing over time. I'll be updating this program little by little as I practice.");
+
+            string options = "1. Convert ºC <--> ºF\n0. EXIT";
+            int input;
+            string final;
+
+            // CLASSES && OBJECTS
+            ConvertCtoF Convertor = new ConvertCtoF();
+
+            do
+            {
+                Console.WriteLine("\nSelect (by a number) the program u want to try:\n");
+                Console.WriteLine(options);
+
+                while (!int.TryParse(Console.ReadLine(), out input) || input <= -1 || input >= 2)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"\nERROR: You wrote a wrong digit. Try it again.");
+                    Console.ResetColor();
+                    Console.WriteLine($"\nThe options are:\n\n{options}");
+                    Console.ResetColor();
+                }
+
+                switch (input)
+                {
+                    case 1:
+                        Convertor.Convertor();
+                        break;
+                    case 0:
+                        Console.WriteLine("\nI hope u enjoyed my mini-project.\nFINISHING...");
+                        return;
+                }
+
+                Console.WriteLine("\nDo you want to execute de program again with other mini-project?\nYes (Y) -  No (N)");
+                final = Console.ReadLine().ToLower();
+                while (final != "yes" && final != "y" && final != "no" && final != "n")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"\nERROR: you must say Yes (Y) or No (N)");
+                    Console.ResetColor();
+                    final = Console.ReadLine().ToLower();
+                }
+            } while (final == "yes" || final == "y");
+
+            Console.WriteLine("\nI hope u enjoyed my mini-project.\nFINISHING...");
         }
     }
 }
